3f162b9e1349c196427e195762c0cd371ba7126d
Ali Madni <ali555m@hotmail.com>
2019-02-10 11:56:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,10 +6,23 @@ def prime?(n)
     true
   else
     ary = [3..n]
-    ary2 = ary.clone
-    num = ary2[0]
-    while num < ary2.size - ary2[0]
+    new_ary = array_changer(ary, [])
+  end
+end
 
-
+def array_changer(array, prime_array)
+  n = array[0]
+  prime_array.push(n)
+  if n**2 > array.last
+    prime_array
+  else
+    n_val = n
+    while n < array.last
+      if array.include?(n)
+        array.delete(n)
+      end
+      n += n_val
+    end
+    array_changer(array, prime_array)
   end
 end
