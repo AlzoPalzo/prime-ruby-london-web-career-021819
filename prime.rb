0e54e448380c991667a1690b8bda2f0df78f6893
Ali Madni <ali555m@hotmail.com>
2019-02-10 11:56:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1
+    false
+  elsif n = 2
+    true
+  else
+    ary = [3..n]
+    ary2 = ary.clone
+    num = ary2[0]
+    while num < ary2.size - ary2[0]
+
+
+  end
+end
